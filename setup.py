37d5c7bde0c861fbec030528e69d9675f2ea8fed
lewis-morris <lewis.morris@gmail.com>
2023-02-11 15:10:54 +0000

    big changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyfindimage',
+    version='0.25',
+    packages=['locater'],
+    url='',
+    license='MIT',
+    author='lewis',
+    author_email='lewis.morris@gmail.com',
+    description='Image finder based on text'
+)
